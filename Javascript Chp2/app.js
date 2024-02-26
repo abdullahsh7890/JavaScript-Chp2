4ad63eed7fa3c26c28890f97b27cfd5245cc2911
Abdullah Bin Shahbaz <154223491+abdullahsh7890@users.noreply.github.com>
2024-02-27 03:08:19 +0500

    Add files via upload

---
+++ Javascript Chp2/app.js
@@ -0,0 +1,32 @@
+//Q1//
+var username
+//Q2//
+var myName = "Abdullah Bin Shahbaz"
+//Q3//
+var message = "Hello World"
+alert (message)
+//Q4//
+var name = "Abdullah"
+alert(name)
+var age ="15 years old"
+alert(age)
+var skill = "Certified Mobile Application Development"
+alert(skill)
+// Q5//
+var pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+
+//Q6//
+var email = "abdullahshahbaz79@gmail.com"
+alert(email)
+
+//Q7//
+var book = "A smarter way to learn JavaScript"
+alert(book)
+
+// Q8 //
+document.write("Yah! I can write HTML content through JavaScript")
+
+//Q9//
+var emoji = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(emoji)
